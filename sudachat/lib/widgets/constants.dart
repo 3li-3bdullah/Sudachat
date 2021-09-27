3dae273680dcdb2e5b93f8ca06d30d940fe861ee
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-27 15:29:59 +0200

    Added to const color

--- sudachat/lib/widgets/constants.dart
+++ sudachat/lib/widgets/constants.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 
 const kSignColor = Color(0xffff8fc8);
 const kRegisterColor = Color(0xff1d5bb5);
+const ksignBPage = Color(0xff6487BD);
+const kregisterBPage = Color(0xfff93f43);
+const kborder = Color(0xfffbbb12);
