98c798995923c3babbdc4cd8b79c3ee1d9115c03
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-27 09:44:57 +0200

    Added padding widget in ModalProgressSUD

--- sudachat/lib/screens/registration_screen.dart
+++ sudachat/lib/screens/registration_screen.dart
@@ -22,76 +22,80 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: 180,
-                child: Image.asset("images/chat.png"),
-              ),
-              SizedBox(height: 50),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: InputDecoration(
-                  hintText: "Enter your Email",
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: kSignColor, width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: kRegisterColor, width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
+      body: ModalProgressHUD(
+         inAsyncCall: showSpinner,
+              child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  height: 180,
+                  child: Image.asset("images/chat.png"),
                 ),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: InputDecoration(
-                  hintText: "Enter your password",
-                  contentPadding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: kSignColor, width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: kRegisterColor, width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                SizedBox(height: 50),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter your Email",
+                    contentPadding:
+                        const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kSignColor, width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kRegisterColor, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              MyButton(
-                  color: kRegisterColor,
-                  title: "Register",
-                  onPressed: () async {
-                    try {
-                      final newUser =
-                          await _auth.createUserWithEmailAndPassword(
-                              email: email, password: password);
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) {
-                        return ChatScreen();
-                      }));
-                    } catch (e) {
-                      print(e);
-                    }
-                  }),
-            ]),
+                SizedBox(height: 8),
+                TextField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter your password",
+                    contentPadding:
+                        const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kSignColor, width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kRegisterColor, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                ),
+                SizedBox(height: 10),
+                MyButton(
+                    color: kRegisterColor,
+                    title: "Register",
+                    onPressed: () async {
+                      
+                      try {
+                        final newUser =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return ChatScreen();
+                        }));
+                      } catch (e) {
+                        print(e);
+                      }
+                    }),
+              ]),
+        ),
       ),
     );
   }
