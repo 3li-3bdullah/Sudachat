23d2cc529ee145ed9fa27d6cec4aada69d6e5a62
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-27 09:58:25 +0200

    Added import modal-progress-hud-nsn.dart and some features

--- sudachat/lib/screens/signin_screen.dart
+++ sudachat/lib/screens/signin_screen.dart
@@ -3,6 +3,7 @@ import '../screens/chat_screen.dart';
 import '../widgets/my_button.dart';
 import '../widgets/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class SignInScreen extends StatefulWidget {
   @override
@@ -13,82 +14,86 @@ class _SignInScreenState extends State<SignInScreen> {
   final _auth = FirebaseAuth.instance;
   late String email;
   late String password;
+  bool showSpinner = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: 180,
-                child: Image.asset("images/chat.png"),
-              ),
-              SizedBox(height: 50),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  email = value;
-                },
-                decoration: InputDecoration(
-                  hintText: "Enter your Email",
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: kSignColor, width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue, width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
+      body: ModalProgressHUD(
+            isAsyncCall:showSpinner,
+              child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  height: 180,
+                  child: Image.asset("images/chat.png"),
                 ),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {
-                  password = value;
-                },
-                decoration: InputDecoration(
-                  hintText: "Enter your password",
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: kSignColor, width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue, width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                SizedBox(height: 50),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter your Email",
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kSignColor, width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              MyButton(
-                  color: kSignColor,
-                  title: "Sign in",
-                  onPressed: () async {
-                    try {
-                      final user = await _auth.signInWithEmailAndPassword(
-                          email: email, password: password);
-                      if (user != null) {
-                        Navigator.of(context)
-                            .push(MaterialPageRoute(builder: (context) {
-                          return ChatScreen();
-                        }));
+                SizedBox(height: 8),
+                TextField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter your password",
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: kSignColor, width: 1),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
+                ),
+                SizedBox(height: 10),
+                MyButton(
+                    color: kSignColor,
+                    title: "Sign in",
+                    onPressed: () async {
+                      try {
+                        final user = await _auth.signInWithEmailAndPassword(
+                            email: email, password: password);
+                        if (user != null) {
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return ChatScreen();
+                          }));
+                        }
+                      } catch (e) {
+                        print(e);
                       }
-                    } catch (e) {
-                      print(e);
-                    }
-                  }),
-            ]),
+                    }),
+              ]),
+        ),
       ),
     );
   }
