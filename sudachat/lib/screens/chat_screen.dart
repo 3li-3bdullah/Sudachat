7071184d3efc74caaa1364f28d2ead5fbb371fab
Ali Abullah <PROGRMMER7@gmail.com>
2021-09-27 14:07:01 +0200

    Added some features to send button

--- sudachat/lib/screens/chat_screen.dart
+++ sudachat/lib/screens/chat_screen.dart
@@ -78,7 +78,8 @@ class _ChatScreenState extends State<ChatScreen> {
                   TextButton(
                     onPressed: () {
                       _firestore.collection('messages').add({
-                        
+                        'text': messageText,
+                        'sender': signedInUser.email,
                       });
                     },
                     child: Text("send",
